3a33a9f9b9e3d219691a2d5d38e2b7c8f25bdfa2
Bogdan Lebedev <lebedevbogdan@gmail.com>
2024-06-05 22:58:36 +0300

    add prometheus and grafana

--- generate_requests.py
+++ generate_requests.py
@@ -1,10 +1,13 @@
 import requests
 import time
+import json
 
-for i in range(3):
-    params = {
-        'x': str(i),
-        'y': '2',
-    }
-    response = requests.get('http://localhost:1702/predict', params=params)
-    time.sleep(10)
+with open('services/models/input_example.json', 'r') as json_file:
+    example = json.load(json_file)
+example_dict = dict(zip(example['columns'], example['data'][0]))
+
+for j in range(50):
+    for i in range(3):
+        params = dict(zip(example['columns'], example['data'][i]))
+        response = requests.post('http://localhost:1702/predict?flat_id=11111', json=params)
+    time.sleep(1)
--- services/ml_service/main.py
+++ services/ml_service/main.py
@@ -3,6 +3,9 @@
 
 from fastapi import FastAPI, Body
 from ml_service.handler import FastApiHandler
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Histogram
+from prometheus_client import Counter
 
 import json
 
@@ -12,11 +15,23 @@ app = FastAPI()
 # Создаем обработчик запросов для API
 app.handler = FastApiHandler()
 
+# инициализируем и запускаем экпортёр метрик
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app)
+
 with open('models/input_example.json', 'r') as json_file:
     example = json.load(json_file)
 example_dict = dict(zip(example['columns'], example['data'][0]))
 
-@app.post("/predict_price/") 
+main_app_predictions = Histogram(
+    "main_app_predictions",
+    "Histogram of predictions",
+    buckets=(1_000_000, 2_000_000, 5_000_000, 10_000_000, 15_000_000, 20_000_000)
+)
+
+main_app_counter_neg = Counter("main_app_counter_neg", "Count of negative predictions")
+
+@app.post("/predict") 
 def get_prediction_for_item(
     flat_id: str,
     model_params: dict = Body(
@@ -36,4 +51,8 @@ def get_prediction_for_item(
         "flat_id": flat_id,
         "model_params": model_params
     }
-    return app.handler.handle(all_params)
+    result = app.handler.handle(all_params)
+    main_app_predictions.observe(result['score'])
+    if result['score'] < 0:
+        main_app_counter_neg.inc()
+    return result
